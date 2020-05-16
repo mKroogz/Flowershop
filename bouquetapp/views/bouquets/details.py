69c8f6e31670fe7b35056835495afcbdd4a7158c
mKroogz <mattk932009@gmail.com>
2020-05-15 20:02:04 -0500

    can now delete flowers from bouquets

--- bouquetapp/views/bouquets/details.py
+++ bouquetapp/views/bouquets/details.py
@@ -10,10 +10,12 @@ def get_bouquet(bouquet_id):
 
         db_cursor.execute("""
             SELECT
+                b.id,
                 b.name,
                 b.occasion,
                 f.name,
-                bf.quantity
+                bf.quantity,
+                bf.id
             FROM bouquetapp_bouquetflower AS bf 
             JOIN bouquetapp_bouquet AS b ON bf.bouquet_id = b.id
             JOIN bouquetapp_flower AS f ON bf.flower_id = f.id
@@ -25,20 +27,34 @@ def get_bouquet(bouquet_id):
 
         for row in dataset:
             bouquet = Bouquet()
-            bouquet.name = row[0]
-            bouquet.occasion = row[1]
-            bouquet.flower = row[2]
-            bouquet.quantity = row[3]
+            bouquet.id = row[0]
+            bouquet.name = row[1]
+            bouquet.occasion = row[2]
+            bouquet.flower_name = row[3]
+            bouquet.quantity = row[4]
+            bouquet.flower = row[5]
 
             all_bouquets.append(bouquet)
 
         return all_bouquets
 
+def remove_flower(flower):
+    with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            DELETE FROM bouquetapp_bouquetflower
+            WHERE id = ?
+            """, (flower,))
+
+
 
 def bouquet_details(request, bouquet_id):
     if request.method == 'GET':
         bouquets = get_bouquet(bouquet_id)
-        info = bouquets[0]
+        info = ""
+        if bouquets:
+            info = bouquets[0]
 
         template = 'bouquets/detail.html'
         context = {
@@ -47,3 +63,15 @@ def bouquet_details(request, bouquet_id):
         }
 
         return render(request, template, context)
+    
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "DELETE"
+            and "flower" in form_data
+        ):
+            remove_flower(form_data["flower"])
+
+        return redirect(reverse('bouquetapp:home'))
